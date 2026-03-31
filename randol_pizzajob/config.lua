663e61a6d612e60dec45f3de81c72e231d98e4c3
Ethereal Developments <155743571+EtherealDevelopments@users.noreply.github.com>
2026-03-31 14:19:52 +0100

    Add files via upload

---
+++ randol_pizzajob/config.lua
@@ -0,0 +1,8 @@
+return {
+    BossModel = `s_f_y_clubbar_01`,
+    BossCoords = vec4(538.42, 101.82, 95.54, 155.16), -- The Blip also uses these coords.
+    FuelScript = {
+        enable = true,
+        script = 'cdn-fuel',
+    },
+}
---
+++ randol_pizzajob/fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Randolio'
+description 'Reworked Pizza Job for ESX/QB.'
+
+shared_scripts {
+    'config.lua',
+    '@ox_lib/init.lua',
+}
+
+server_scripts {
+    'bridge/server/**.lua',
+    'sv_config.lua',
+    'sv_pizzajob.lua'
+}
+
+client_scripts {
+    'bridge/client/**.lua',
+    'cl_pizzajob.lua'
+}
+
+lua54 'yes'
+
+escrow_ignore { 
+'config.lua',
+'sv_config.lua',
+
+} 
+
+dependency '/assetpacks'
